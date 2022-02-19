5d1fa77cb0a0bc9f3d501c68f1b9cadde59316d4
Clarence Chan <clarencechaan@icloud.com>
2022-02-19 15:16:55 -0500

    completed etch-a-sketch

---
+++ script.js
@@ -0,0 +1,80 @@
+const container = document.createElement('div');
+container.classList.add('container');
+document.body.appendChild(container);
+
+let size = 16;
+let penStyle = 'solid'
+
+function clearGrid() {
+
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.style.backgroundColor = '#E3FDFD';
+        square.style.opacity = null;
+    });;
+
+}
+
+
+function createGrid() {
+
+    const rows = document.querySelectorAll('.row');
+    rows.forEach((row) => {
+        row.remove();
+    });
+
+    for (let i = 0; i < size; i++) {
+            const row = document.createElement('div');
+            for (let j = 0; j < size; j++) {
+                const square = document.createElement('div');
+                row.classList.add('row')
+                square.classList.add('square')
+
+                square.addEventListener('mouseover', () => {
+                    if (penStyle === 'solid') {
+                        square.style.backgroundColor = '#71C9CE';
+                        square.style.opacity = 1;
+                    } else if (penStyle === 'rainbow') {
+                        let randomColor = Math.floor(Math.random()*16777215).toString(16);
+                        square.style.backgroundColor = '#' + randomColor;
+                    } else if (penStyle === 'translucent') {
+                        square.style.backgroundColor = '#71C9CE';
+                        if (parseFloat(square.style.opacity) > 0) {
+                            square.style.opacity = parseFloat(square.style.opacity) + 0.25;
+                        } else {
+                            square.style.opacity = .25;
+                        }
+                    }
+                });
+
+                row.appendChild(square);
+                container.appendChild(row);
+            }
+        }
+}
+
+function promptForSize() {
+    do {
+        size = prompt("Enter number of squares per side between 1 and 100");
+    } while (size > 100 || size < 1 || isNaN(size) || size === "" || size === null);
+}
+
+const clearBtn = document.querySelector('#clear');
+clearBtn.addEventListener('click', clearGrid);
+
+const penRadios = document.querySelectorAll('input[name=pen-style]');
+penRadios.forEach((radio) => {
+    radio.addEventListener('change', () => {
+        penStyle = radio.value;
+    });
+}); 
+
+const sizeForm = document.querySelector('#size');
+sizeForm.addEventListener('keyup', () => {
+    if (parseInt(sizeForm.value) >= 1 && parseInt(sizeForm.value) <= 100) {
+        size = parseInt(sizeForm.value);
+        createGrid();
+    }
+});
+
+createGrid();
